4b88afda187793b7a047c58949ca7e33ab391252
yziquel <yziquel@8462dd83-5746-40b0-b0c8-3ca2f889d9d2>
2009-11-13 19:20:36 +0000

    Tidying up rstdenv.R output.
    git-svn-id: svn+ssh://svn.gna.org/svn/ocaml-r/trunk@39 8462dd83-5746-40b0-b0c8-3ca2f889d9d2

--- src/rstdenv.R
+++ src/rstdenv.R
@@ -1,14 +1,6 @@
 ExcludeVars <- c("R_SESSION_TMPDIR","R_HISTFILE")
 IncludeVars <- Sys.getenv()
 IncludeVars <- IncludeVars[grep("^R_",names(IncludeVars),perl=TRUE)]
-cat("(** Functor to properly set up R environment variables. *)\n")
-cat("\n")
-cat("module type Environment =\n")
-cat("sig\n")
-cat("  val env : (string * string) list\n")
-cat("end\n")
-cat("\n")
-cat("module Standard : Environment = struct\n")
 cat("  let env = [\n")
 for (i in 1:length(IncludeVars)){
 	if (names(IncludeVars)[i] %in% ExcludeVars)
@@ -16,4 +8,3 @@ for (i in 1:length(IncludeVars)){
 	cat('    "',names(IncludeVars)[i],'", "',IncludeVars[i],'";\n',sep='')
 }
 cat("  ]\n")
-cat("end\n\n")
