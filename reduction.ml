9b51ca58453bf69b70b44c02e3b7115a92d3d481
yziquel <yziquel@8462dd83-5746-40b0-b0c8-3ca2f889d9d2>
2009-12-03 11:21:06 +0000

    New semantics for R.eval.
    git-svn-id: svn+ssh://svn.gna.org/svn/ocaml-r/trunk@176 8462dd83-5746-40b0-b0c8-3ca2f889d9d2

--- reduction.ml
+++ reduction.ml
@@ -2,8 +2,20 @@ external eval_langsxp : lang sxp -> sexp = "r_eval_sxp"
 
 let eval_string s = eval_langsxp (parse_sexp s)
 
+let rec prepare_args = function
+  | (Some x)::l -> x::(prepare_args l)
+  | None::l     -> prepare_args l
+  | []          -> []
+
+let arg f x = Some (f x)
+let opt f x = match x with
+  | None -> None
+  | Some xx -> Some (f xx)
+
 let eval (l : sexp list) : sexp =
     (* The typing of l needs to be amended and moved to 'a t stuff. *)
     eval_langsxp (langsxp_of_list l (List.length l))
 
-let mlfun (f: ('a -> 'b) t) (x: 'a t) : 'b t = eval [f; x]
+let eval phi args =
+  let l = phi::(prepare_args args) in
+  eval_langsxp (langsxp_of_list l (List.length l))
