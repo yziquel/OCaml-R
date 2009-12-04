fa69bd2298107df3ab1934411a04afcf9f3a1fe1
yziquel <yziquel@8462dd83-5746-40b0-b0c8-3ca2f889d9d2>
2009-12-04 13:53:17 +0000

    Documenting.
    git-svn-id: svn+ssh://svn.gna.org/svn/ocaml-r/branches/yziquel@193 8462dd83-5746-40b0-b0c8-3ca2f889d9d2

--- read_internal.c
+++ read_internal.c
@@ -24,121 +24,330 @@
    CHARSXP cache. */
 
 
+/**  Returns the length of a VECSEXP.
+  *
+  *  inspect_vecsxp_length takes a VECSEXP as argument and
+  *  returns its length, i.e., the number of elements.
+  */
+
 CAMLprim value inspect_vecsxp_length (value vecsexp) {
   CAMLparam1(vecsexp);
-  CAMLreturn(Val_int(Vecsexp_val(vecsexp)->vecsxp.length));
+  CAMLlocal1(result);
+  result = Val_int(LENGTH(Vecsexp_val(vecsexp)));
+  CAMLreturn(result);
 }
+
 
 /*CAMLprim value inspect_vecsxp_truelength (value vecsexp) {
   CAMLparam1(vecsexp);
-  CAMLreturn(Val_int(Vecsexp_val(vecsexp)->vecsxp.truelength));
+  CAMLlocal1(result);
+  result = Val_int(TRUELENGTH(Vecsexp_val(vecsexp)));
+  CAMLreturn(result);
 }*/
+
+
 
 /* Concerning various types of SEXPs: */
 
+
+/**  Returns the offset at the primitive can be found.
+  *
+  *  inspect_primsxp_offset take a SEXP denoting a function primitive
+  *  as argument, and returns its offset in the table of primitives.
+  *
+  *  Note: This function bypasses the provided API, and requires the
+  *  use of the #define USE_RINTERNALS directive.
+  */
+
 CAMLprim value inspect_primsxp_offset (value sexp) {
   CAMLparam1(sexp);
-  CAMLreturn(Val_int(Sexp_val(sexp)->u.primsxp.offset));
+  CAMLlocal1(result);
+  result = Val_int(Sexp_val(sexp)->u.primsxp.offset);
+  CAMLreturn(result);
 }
+
+
+/**  Returns the name of a symbol.
+  *
+  *  inspect_symsxp_pname take a SYMSXP as argument, and returns the
+  *  SEXP containing its name.
+  */
 
 CAMLprim value inspect_symsxp_pname (value sexp) {
   CAMLparam1(sexp);
-  CAMLreturn(Val_sexp(Sexp_val(sexp)->u.symsxp.pname));
+  CAMLlocal1(result);
+  result = Val_sexp(PRINTNAME(Sexp_val(sexp)));
+  CAMLreturn(result);
 }
+
+
+/**  Returns the value of a symbol.
+  *
+  *  inspect_symsxp_value takes a SYMXSP as argument, and returns the
+  *  SEXP containing its value.
+  */
 
 CAMLprim value inspect_symsxp_value (value sexp) {
   CAMLparam1(sexp);
-  CAMLreturn(Val_sexp(Sexp_val(sexp)->u.symsxp.value));
+  CAMLlocal1(result);
+  result = Val_sexp(SYMVALUE(Sexp_val(sexp)));
+  CAMLreturn(result);
 }
+
+
+/**  Returns the internal sexp of a symbol.
+  *
+  *  inspect_symsxp_internal takes a SYMSXP as argument, and returns the
+  *  SEXP containing its internal value.
+  */
 
 CAMLprim value inspect_symsxp_internal (value sexp) {
   CAMLparam1(sexp);
-  CAMLreturn(Val_sexp(Sexp_val(sexp)->u.symsxp.internal));
+  CAMLlocal1(result);
+  result = Val_sexp(INTERNAL(Sexp_val(sexp)));
+  CAMLreturn(result);
 }
+
+
+/**  Returns the head element of a pairlist.
+  *
+  *  inspect_listsxp_carval takes a pairlist as argument, and
+  *  returns the SEXP containing its head element.
+  */
 
 CAMLprim value inspect_listsxp_carval (value sexp) {
   CAMLparam1(sexp);
-  CAMLreturn(Val_sexp(Sexp_val(sexp)->u.listsxp.carval));
+  CAMLlocal1(result);
+  result = Val_sexp(CAR(Sexp_val(sexp)));
+  CAMLreturn(result);
 }
+
+
+/**  Returns the tail pairlist of a pairlist.
+  *
+  *  inspect_listsxp_cdrval takes a pairlist as argument,
+  *  and returns its tail pairlist.
+  */
 
 CAMLprim value inspect_listsxp_cdrval (value sexp) {
   CAMLparam1(sexp);
-  CAMLreturn(Val_sexp(Sexp_val(sexp)->u.listsxp.cdrval));
+  CAMLlocal1(result);
+  result = Val_sexp(CDR(Sexp_val(sexp)));
+  CAMLreturn(result);
 }
+
+
+/**  Returns the tag value of the head element of a pairlist.
+  *
+  *  inspect_listsxp_tagval takes a pairlist as argument, and
+  *  returns the tag value of the head element of the pairlist.
+  */
 
 CAMLprim value inspect_listsxp_tagval (value sexp) {
   CAMLparam1(sexp);
-  CAMLreturn(Val_sexp(Sexp_val(sexp)->u.listsxp.tagval));
+  CAMLlocal1(result);
+  result = Val_sexp(TAG(Sexp_val(sexp)));
+  CAMLreturn(result);
 }
+
+
+/**  Returns the frame of an evironment.
+  *
+  *  inspect_envsxp_frame takes an environment as argument,
+  *  and returns the frame of this environment.
+  */
 
 CAMLprim value inspect_envsxp_frame (value sexp) {
   CAMLparam1(sexp);
-  CAMLreturn(Val_sexp(Sexp_val(sexp)->u.envsxp.frame));
+  CAMLlocal1(result);
+  result = Val_sexp(FRAME(Sexp_val(sexp)));
+  CAMLreturn(result);
 }
+
+
+/**  Returns the enclosing environment of an environment.
+  *
+  *  inspect_envsxp_enclos takes an environment as argument,
+  *  and returns its enclosing environment.
+  */
 
 CAMLprim value inspect_envsxp_enclos (value sexp) {
   CAMLparam1(sexp);
-  CAMLreturn(Val_sexp(Sexp_val(sexp)->u.envsxp.enclos));
+  CAMLlocal1(result);
+  result = Val_sexp(ENCLOS(Sexp_val(sexp)));
+  CAMLreturn(result);
 }
+
+
+/**  Returns the hash table of an environment.
+  *
+  *  inspect_envsxp_hashtabl takes an environment as argument,
+  *  and returns its hash table.
+  */
 
 CAMLprim value inspect_envsxp_hashtab (value sexp) {
   CAMLparam1(sexp);
-  CAMLreturn(Val_sexp(Sexp_val(sexp)->u.envsxp.hashtab));
+  CAMLlocal1(result);
+  result = Val_sexp(HASHTAB(Sexp_val(sexp)));
+  CAMLreturn(result);
 }
+
+
+/**  Returns the list of formal arguments of a closure.
+  *
+  *  inspect_closxp_formals takes a closure as argument,
+  *  and returns the list of its formal arguments.
+  */
 
 CAMLprim value inspect_closxp_formals (value sexp) {
   CAMLparam1(sexp);
-  CAMLreturn(Val_sexp(Sexp_val(sexp)->u.closxp.formals));
+  CAMLlocal1(result);
+  result = Val_sexp(FORMALS(Sexp_val(sexp)));
+  CAMLreturn(resut);
 }
+
+
+/**  Returns the body of a closure.
+  *
+  *  inspect_closxp_body takes a closure as argument,
+  *  and returns the body of this closure.
+  */
 
 CAMLprim value inspect_closxp_body (value sexp) {
   CAMLparam1(sexp);
-  CAMLreturn(Val_sexp(Sexp_val(sexp)->u.closxp.body));
+  CAMLlocal1(result);
+  result = Val_sexp(BODY(Sexp_val(sexp)));
+  CAMLreturn(result);
 }
+
+
+/**  Returns the environment of a closure.
+  *
+  *  inspect_closxp_env takes a closure as argument,
+  *  and returns the environment of this closure.
+  */
 
 CAMLprim value inspect_closxp_env (value sexp) {
   CAMLparam1(sexp);
-  CAMLreturn(Val_sexp(Sexp_val(sexp)->u.closxp.env));
+  CAMLlocal1(result);
+  result = Val_sexp(CLOENV(Sexp_val(sexp)));
+  CAMLreturn(result);
 }
+
+
+/**  Returns the value of a promise.
+  *
+  *  inspect_promsxp_value takes a promise as argument,
+  *  and returns the value of this promise.
+  *
+  *  Note: This function bypasses the provided API, and requires the
+  *  use of the #define USE_RINTERNALS directive.
+  */
 
 CAMLprim value inspect_promsxp_value (value sexp) {
   CAMLparam1(sexp);
-  CAMLreturn(Val_sexp(Sexp_val(sexp)->u.promsxp.value));
+  CAMLlocal1(result);
+  result = Val_sexp(Sexp_val(sexp)->u.promsxp.value);
+  CAMLreturn(result);
 }
+
+
+/**  Returns the expression of a promise.
+  *
+  *  inspect_promsxp_expr takes a promise as argument,
+  *  and returns the value of this promise.
+  *
+  *  Note: This function bypasses the provided API, and requires the
+  *  use of the #define USE_RINTERNALS directive.
+  */
 
 CAMLprim value inspect_promsxp_expr (value sexp) {
   CAMLparam1(sexp);
-  CAMLreturn(Val_sexp(Sexp_val(sexp)->u.promsxp.expr));
+  CAMLlocal1(result);
+  result = Val_sexp(Sexp_val(sexp)->u.promsxp.expr);
+  CAMLreturn(result);
 }
+
+
+/**  Returns the environment of a promise.
+  *
+  *  inspect_promsxp_env takes a promise as argument,
+  *  and returns the environment of this promise.
+  *
+  *  Note: This function bypasses the provided API, and requires the
+  *  use of the #define USE_RINTERNALS directive.
+  */
 
 CAMLprim value inspect_promsxp_env (value sexp) {
   CAMLparam1(sexp);
-  CAMLreturn(Val_sexp(Sexp_val(sexp)->u.promsxp.env));
+  CAMLlocal1(result);
+  result = Val_sexp(Sexp_val(sexp)->u.promsxp.env);
+  CAMLreturn(result);
 }
+
+
+/**  Returns an element of a logical vector.
+  *
+  *  r_access_lgl_vecsxp takes a logical vector as argument,
+  *  and an offset, and returns the element at this offset.
+  */
 
 CAMLprim value r_access_lgl_vecsxp (value lglsxp, value offset) {
   CAMLparam2(lglsxp, offset);
-  CAMLreturn(Val_bool(LOGICAL((int *) Vecsexp_val(lglsxp))[Int_val(offset)]));
+  CAMLlocal1(result);
+  result = Val_bool(LOGICAL((int *) Vecsexp_val(lglsxp))[Int_val(offset)]);
+  CAMLreturn(result);
 }
+
+
+/**  Returns an element of a vector of integers.
+  *
+  *  r_access_int_vecsxp takes a vector of integers as argument,
+  *  and an offset, and returns the element at this offset.
+  */
 
 CAMLprim value r_access_int_vecsxp (value intsxp, value offset) {
   CAMLparam2(intsxp, offset);
+  CAMLlocal1(result);
+
   /* The R macro is #define INTEGER(x) ((int *) DATAPTR(x)).
      Should use Val_int, or int32s? More generally, the typing
      is here somewhat confusing (or confused)... Is offset an int? */
-  CAMLreturn(Val_int(INTEGER((int *) Vecsexp_val(intsxp))[Int_val(offset)]));
+
+  result = Val_int(INTEGER((int *) Vecsexp_val(intsxp))[Int_val(offset)]);;
+  CAMLreturn(result);
 }
+
+
+/**  Returns an element of a vector of strings.
+  *
+  *  r_access_str_vecsxp takes a vector of strings as argument,
+  *  and an offset, and returns the element at this offset.
+  */
 
 CAMLprim value r_access_str_vecsxp (value strsxp, value offset) {
   CAMLparam2(strsxp, offset);
-  /* Same comments as for r_accesas_int_vecsxp and for
+  CAMLlocal1(result);
+
+  /* Same comments as for r_access_int_vecsxp and for
      r_internal_string_of_charsxp. */
-  CAMLreturn(caml_copy_string(CHAR(STRING_ELT(
-    (char **) Vecsexp_val(strsxp), (Int_val(offset))))));
+
+  result = caml_copy_string(CHAR(STRING_ELT((char **)
+    Vecsexp_val(strsxp), (Int_val(offset)))));
+  CAMLreturn(result);
 }
+
+
+/**  Returns an element of a vector of SEXPs.
+  *
+  *  r_access_sexp_vecsxp takes a vector of SEXPs as argument,
+  *  and an offset, and returns the element at this offset.
+  */
 
 CAMLprim value r_access_sexp_vecsxp (value sexpsxp, value offset) {
   CAMLparam2(sexpsxp, offset);
+  CAMLlocal1(result);
   SEXP * v = (SEXP *) Vecsexp_val(sexpsxp);
-  CAMLreturn(Val_sexp(VECTOR_ELT(v,Int_val(offset))));
+  result = Val_sexp(VECTOR_ELT(v, Int_val(offset)));
+  CAMLreturn(result);
 }
