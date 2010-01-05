867093f23e188af4a701cdd608f27f3054b3809b
yziquel <yziquel@8462dd83-5746-40b0-b0c8-3ca2f889d9d2>
2010-01-05 18:37:12 +0000

    Opening up the interface.
    git-svn-id: svn+ssh://svn.gna.org/svn/ocaml-r/branches/yziquel@208 8462dd83-5746-40b0-b0c8-3ca2f889d9d2

--- conversion.mli
+++ conversion.mli
@@ -1,5 +1,7 @@
 val bool : bool -> bool vecsxp
 
+val float_list_of_real_vecsxp : sexp -> float list
+
 val strings_of_t : string list t -> string list
 val string : string -> string vecsxp
 val strings : string list -> string vecsxp
--- sexptype.mli
+++ sexptype.mli
@@ -7,3 +7,34 @@ type 'a vec
 type 'a vecsxp = 'a vec sxp
 
 val sexp : 'a sxp -> sexp
+
+(* Algebraic type reflecting R's dynamic typing. *)
+type sexptype =
+  | NilSxp
+  | SymSxp
+  | ListSxp
+  | CloSxp
+  | EnvSxp
+  | PromSxp
+  | LangSxp
+  | SpecialSxp
+  | BuiltinSxp
+  | CharSxp
+  | LglSxp
+  | IntSxp
+  | RealSxp
+  | CplxSxp
+  | StrSxp
+  | DotSxp
+  | AnySxp
+  | VecSxp
+  | ExprSxp
+  | BcodeSxp
+  | ExtptrSxp
+  | WeakrefSxp
+  | RawSxp
+  | S4Sxp
+  | FunSxp
+
+val sexptype : sexp -> sexptype
+
