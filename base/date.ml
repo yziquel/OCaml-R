41e8fdc91b41d96d062ff35ab05256ea40da7bad
yziquel <yziquel@8462dd83-5746-40b0-b0c8-3ca2f889d9d2>
2010-01-11 00:20:17 +0000

    Conversion of R dates to OCaml Calendar dates.
    git-svn-id: svn+ssh://svn.gna.org/svn/ocaml-r/branches/yziquel@239 8462dd83-5746-40b0-b0c8-3ca2f889d9d2

--- base/date.ml
+++ base/date.ml
@@ -3,11 +3,13 @@ module Date = struct
   class type t = object
     inherit S3.t
     method as_float : float
+    method as_date : CalendarLib.Calendar.Date.t
   end
 
-  class from_R r : t = object
+  class from_R r : t = object (self)
     inherit S3.from_R r
     method as_float = float_of_t underlying
+    method as_date = Calendar.Date.from_unixfloat (86400. *. self#as_float)
   end
 
   let t_from_R r : t = new from_R r
--- base/date.mli
+++ base/date.mli
@@ -3,6 +3,7 @@ module Date : sig
   class type t = object
     inherit S3.t
     method as_float : float
+    method as_date : CalendarLib.Calendar.Date.t
   end
 
   val t_from_R : sexp -> t
