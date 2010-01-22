253e59c69d4817f28a3025d1eadbfea97625929f
yziquel <yziquel@8462dd83-5746-40b0-b0c8-3ca2f889d9d2>
2010-01-22 20:22:26 +0000

    Documenting S3 classes.
    git-svn-id: svn+ssh://svn.gna.org/svn/ocaml-r/branches/yziquel@253 8462dd83-5746-40b0-b0c8-3ca2f889d9d2

--- s3.mli
+++ s3.mli
@@ -1,14 +1,31 @@
+(* Wrapping S3 classes into Objective Caml. *)
+
 module S3 : sig
 
+  (**  Class type for S3 objects in R. *)
   class type t = object
+
     method underlying : sexp
+    (**  Access to the underlying R data structure. *)
+
     method attribute  : string -> sexp
+    (**  [attribute attr_name] returns the R data structure
+      *  which is the object's attribute of name [attr_name]. *)
+
     method attributes : (Specification.symbol * sexp) list
+    (**  Returns the whole list of attributes of an S3 object. *)
+
     method classes    : string list
+    (**  Returns the list of S3 classes that the object is
+      *  an instance of. *)
+
   end
 
   class from_R : sexp -> t
+  (**  Constructor of an [S3.t] object from an R S3 object. *)
+
   val t_from_R : sexp -> t
+  (**  A handy shortcut for the [from_R] class constructor. *)
 
 end
 
