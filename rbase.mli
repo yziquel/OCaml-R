5826bbb0aff5f43fc10f631ac9498e3dac65990c
yziquel <yziquel@8462dd83-5746-40b0-b0c8-3ca2f889d9d2>
2010-01-23 14:36:56 +0000

    Removed intermediary file.
    git-svn-id: svn+ssh://svn.gna.org/svn/ocaml-r/branches/yziquel@256 8462dd83-5746-40b0-b0c8-3ca2f889d9d2

--- rbase.mli
+++
@@ -1,8 +0,0 @@
-val sample : 'a list R.t -> int -> ?replace: bool -> ?prob: float list -> unit -> 'a list R.t
-
-class listing_from_R : R.Base.listing R.t -> R.Base.listing
-
-class dataframe_from_R : R.Base.dataframe R.t -> R.Base.dataframe
-
-class date_from_R : R.Base.date R.t -> R.Base.date
-
