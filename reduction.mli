fd1f0cdca55edebc0049608da43c47ec710b6d51
yziquel <yziquel@8462dd83-5746-40b0-b0c8-3ca2f889d9d2>
2010-01-29 22:04:03 +0000

    Added R.eval_langsxp to interface.
    git-svn-id: svn+ssh://svn.gna.org/svn/ocaml-r/branches/yziquel@287 8462dd83-5746-40b0-b0c8-3ca2f889d9d2

--- reduction.mli
+++ reduction.mli
@@ -2,22 +2,23 @@
 
 exception Runtime_error of lang sxp * string
 
+val eval_langsxp : lang sxp -> 'a t
+(**  [eval_langsxp] takes a R value containing an R executable expression.
+  *  Also known as a [LANGSXP]. You get the resulting value back. *)
+
 val eval_string : string -> 'a t
-(**  [eval_string] take a string containing R code, and feeds it to the
+(**  [eval_string] takes a string containing R code, and feeds it to the
   *  R interpreter. You get the resulting value back. The typing of this
   *  function is deliberately unsafe in order to allow the user to type
-  *  it precisely.
-  *)
+  *  it precisely. *)
 
 val arg : ('a -> 'b t) -> ?name:string -> 'a -> (string option * sexp) option
 (**  Convenience function to wrap up arguments, when mapping R functions
-  *  to Objective Caml functions.
-  *)
+  *  to Objective Caml functions. *)
 
 val opt : ('a -> 'b t) -> string -> 'a option -> (string option * sexp) option
 (**  Convenience function to wrap up optional arguments, when mapping R functions
-  *  to Objective Caml functions.
-  *)
+  *  to Objective Caml functions. *)
 
 val eval : sexp -> (string option * sexp) option list -> 'a t
 (**  [eval f args] evaluates an the R function [f] with respect to a list of
