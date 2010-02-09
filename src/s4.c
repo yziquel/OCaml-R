b2c3b9c4b268a85b8cabafc7a5be4d2f73386e2a
zoggy <zoggy@8462dd83-5746-40b0-b0c8-3ca2f889d9d2>
2010-02-09 08:23:11 +0000

    add headers
    git-svn-id: svn+ssh://svn.gna.org/svn/ocaml-r/branches/yziquel@307 8462dd83-5746-40b0-b0c8-3ca2f889d9d2

--- src/s4.c
+++ src/s4.c
@@ -1,3 +1,30 @@
+/*********************************************************************************/
+/*                OCaml-R                                                        */
+/*                                                                               */
+/*    Copyright (C) 2008-2010 Institut National de Recherche en                  */
+/*    Informatique et en Automatique. All rights reserved.                       */
+/*                                                                               */
+/*    Copyright (C) 2009-2010 Guillaume Yziquel. All rights reserved.            */
+/*                                                                               */
+/*    This program is free software; you can redistribute it and/or modify       */
+/*    it under the terms of the GNU General Public License as                    */
+/*    published by the Free Software Foundation; either version 3 of the         */
+/*    License, or  any later version.                                            */
+/*                                                                               */
+/*    This program is distributed in the hope that it will be useful,            */
+/*    but WITHOUT ANY WARRANTY; without even the implied warranty of             */
+/*    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the               */
+/*    GNU Library General Public License for more details.                       */
+/*                                                                               */
+/*    You should have received a copy of the GNU General Public                  */
+/*    License along with this program; if not, write to the Free Software        */
+/*    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA                   */
+/*    02111-1307  USA                                                            */
+/*                                                                               */
+/*    Contact: Maxence.Guesdon@inria.fr                                          */
+/*             guillaume.yziquel@citycable.ch                                    */
+/*********************************************************************************/
+
 CAMLprim value ocamlr_is_s4_object (value ml_s4) {
   return(Val_bool(IS_S4_OBJECT(Sexp_val(ml_s4))));
 }
